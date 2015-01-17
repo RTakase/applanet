4489f451dfd46868a51ea1e0c3d7da3dddd5069b
Ryohe <dojise@yahoo.co.jp>
2015-01-17 21:57:10 +0900

    temporary debug edit

--- lib/GooglePlayScraper.rb
+++ lib/GooglePlayScraper.rb
@@ -155,23 +155,20 @@ class GooglePlayScraper
 
     tmp = nil
 
-    puts "by takase, " + "1"
-
     #「類似のアプリ」と「このデベロッパーの他のアプリ」からなる長さ2のノードセット
     nodeset = @webPage
       .xpath("//div[@class='rec-cluster']")
 
-    puts "by takase, " + "2"
-
     #divの日本語のタイトルでどちらが「類似のアプリ」であるかを判断
     nodeset.each do |node|
+      puts "by takase, " + node.xpath("h1").inner_text
+      puts "by takase, " + @similarAppsRep
       if (node.xpath("h1").inner_text == @similarAppsRep)
         tmp = node
         break;
       end
     end
 
-    puts "by takase, " + "3"    
     packageIds = []
 
     #1類似のアプリのパッケージID1ノードなリノードセット
@@ -183,8 +180,6 @@ class GooglePlayScraper
     nodeset.each do |node|      
       packageIds.push(node.value)
     end
-
-    puts "by takase, " + "1"
 
     return packageIds
 
